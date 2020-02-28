eea92c2f93be0985f4a5d9c8b313c27fbd4d6b3a
Bera <wusucuma@gmail.com>
2020-02-28 12:41:21 +0900

    h2 적용

---
+++ src/main/java/com/bera/webflux/config/H2Config.java
@@ -0,0 +1,21 @@
+package com.bera.webflux.config;
+
+import org.h2.tools.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.sql.SQLException;
+
+@Configuration
+public class H2Config {
+
+    @Bean
+    public Server h2TcpServer() throws SQLException {
+        return Server.createTcpServer().start();
+    }
+
+    @Bean
+    public Server h2WebServer() throws SQLException {
+        return Server.createWebServer().start();
+    }
+}
